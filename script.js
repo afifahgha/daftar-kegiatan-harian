2b1110fc455104fe22ae238e34649e8187870e73
afifahgha <afifahgholimatunazzali@gmail.com>
2025-10-16 05:51:47 +0700

    membuat daftar harian

---
+++ script.js
@@ -0,0 +1,22 @@
+const form = document.getElementById("itemForm");
+const input = document.getElementById("itemInput");
+const errorMessage = document.getElementById("errorMessage");
+const list = document.getElementById("daftar");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    const itemText = input.value.trim();
+    if (itemText === "") {
+        errorMessage.textContent = "Field tidak boleh kosong"; 
+        input.classList.add("invalid");
+        return; 
+    } else {
+        errorMessage.textContent = ""; 
+        input.classList.remove("invalid");
+        input.classList.add("valid"); 
+        const newItem = document.createElement("li"); 
+        newItem.textContent = itemText; 
+        list.append(newItem); 
+        input.value = ""; 
+    }
+});
